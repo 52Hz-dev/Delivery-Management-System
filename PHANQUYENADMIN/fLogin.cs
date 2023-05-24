01caf3fe6d419102042d9fa7074305fa9887879f
Script <huutai1515225@gmail.com>
2023-05-24 15:07:50 +0700

    12

--- PHANQUYENADMIN/fLogin.cs
+++ PHANQUYENADMIN/fLogin.cs
@@ -74,7 +74,7 @@ namespace PHANQUYENADMIN
                 nhanvien.ShowDialog();
                 this.Show();
             }
-            // New code1
+            // New code123
             if (role == "Ban giam doc")
             {
                 //
