a77f7e385304007bb25a07a4e1f5c40c0397b77b
Script <huutai1515225@gmail.com>
2023-05-29 22:33:42 +0700

    fix

--- PHANQUYENADMIN/fThongTinNhanVien_QL.Designer.cs
+++ PHANQUYENADMIN/fThongTinNhanVien_QL.Designer.cs
@@ -73,9 +73,8 @@
             this.panel1.Controls.Add(this.tabControl1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1154, 834);
+            this.panel1.Size = new System.Drawing.Size(1026, 667);
             this.panel1.TabIndex = 1;
             // 
             // tabControl1
@@ -84,20 +83,18 @@
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
-            this.tabControl1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1154, 834);
+            this.tabControl1.Size = new System.Drawing.Size(1026, 667);
             this.tabControl1.TabIndex = 0;
             // 
             // ThongTin
             // 
             this.ThongTin.Controls.Add(this.panel2);
-            this.ThongTin.Location = new System.Drawing.Point(4, 38);
-            this.ThongTin.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.ThongTin.Location = new System.Drawing.Point(4, 34);
             this.ThongTin.Name = "ThongTin";
-            this.ThongTin.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.ThongTin.Size = new System.Drawing.Size(1146, 792);
+            this.ThongTin.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.ThongTin.Size = new System.Drawing.Size(1018, 629);
             this.ThongTin.TabIndex = 0;
             this.ThongTin.Text = "ThongTin";
             this.ThongTin.UseVisualStyleBackColor = true;
@@ -108,10 +105,9 @@
             this.panel2.Controls.Add(this.panel5);
             this.panel2.Controls.Add(this.panel4);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(3, 4);
-            this.panel2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel2.Location = new System.Drawing.Point(3, 3);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1140, 784);
+            this.panel2.Size = new System.Drawing.Size(1012, 623);
             this.panel2.TabIndex = 0;
             // 
             // label10
@@ -119,9 +115,9 @@
             this.label10.AutoSize = true;
             this.label10.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(772, 81);
+            this.label10.Location = new System.Drawing.Point(686, 57);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(150, 32);
+            this.label10.Size = new System.Drawing.Size(127, 29);
             this.label10.TabIndex = 10;
             this.label10.Text = "Phân công";
             // 
@@ -130,18 +126,17 @@
             this.panel5.Controls.Add(this.lbNoti);
             this.panel5.Controls.Add(this.dvgPhanCong);
             this.panel5.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel5.Location = new System.Drawing.Point(772, 113);
-            this.panel5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel5.Location = new System.Drawing.Point(686, 86);
             this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(368, 671);
+            this.panel5.Size = new System.Drawing.Size(326, 537);
             this.panel5.TabIndex = 1;
             // 
             // lbNoti
             // 
             this.lbNoti.AutoSize = true;
-            this.lbNoti.Location = new System.Drawing.Point(42, 275);
+            this.lbNoti.Location = new System.Drawing.Point(37, 220);
             this.lbNoti.Name = "lbNoti";
-            this.lbNoti.Size = new System.Drawing.Size(270, 29);
+            this.lbNoti.Size = new System.Drawing.Size(223, 25);
             this.lbNoti.TabIndex = 1;
             this.lbNoti.Text = "Hiện chưa có phân công";
             // 
@@ -155,12 +150,11 @@
             this.dvgPhanCong.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
             this.dvgPhanCong.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dvgPhanCong.Location = new System.Drawing.Point(0, 0);
-            this.dvgPhanCong.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dvgPhanCong.Name = "dvgPhanCong";
             this.dvgPhanCong.RowHeadersVisible = false;
             this.dvgPhanCong.RowHeadersWidth = 51;
             this.dvgPhanCong.RowTemplate.Height = 24;
-            this.dvgPhanCong.Size = new System.Drawing.Size(368, 671);
+            this.dvgPhanCong.Size = new System.Drawing.Size(326, 537);
             this.dvgPhanCong.TabIndex = 0;
             // 
             // panel4
@@ -188,27 +182,26 @@
             this.panel4.Controls.Add(this.lbDiaChi);
             this.panel4.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel4.Location = new System.Drawing.Point(0, 0);
-            this.panel4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(772, 784);
+            this.panel4.Size = new System.Drawing.Size(686, 623);
             this.panel4.TabIndex = 0;
             this.panel4.Paint += new System.Windows.Forms.PaintEventHandler(this.panel4_Paint);
             // 
             // lbMaNV
             // 
             this.lbMaNV.AutoSize = true;
-            this.lbMaNV.Location = new System.Drawing.Point(520, 152);
+            this.lbMaNV.Location = new System.Drawing.Point(462, 122);
             this.lbMaNV.Name = "lbMaNV";
-            this.lbMaNV.Size = new System.Drawing.Size(113, 29);
+            this.lbMaNV.Size = new System.Drawing.Size(94, 25);
             this.lbMaNV.TabIndex = 21;
             this.lbMaNV.Text = "Unknown";
             // 
             // lbql4
             // 
             this.lbql4.AutoSize = true;
-            this.lbql4.Location = new System.Drawing.Point(43, 480);
+            this.lbql4.Location = new System.Drawing.Point(38, 384);
             this.lbql4.Name = "lbql4";
-            this.lbql4.Size = new System.Drawing.Size(68, 29);
+            this.lbql4.Size = new System.Drawing.Size(59, 25);
             this.lbql4.TabIndex = 19;
             this.lbql4.Text = "SDT:";
             this.lbql4.Click += new System.EventHandler(this.label12_Click);
@@ -216,82 +209,81 @@
             // lbPhuCap
             // 
             this.lbPhuCap.AutoSize = true;
-            this.lbPhuCap.Location = new System.Drawing.Point(559, 480);
+            this.lbPhuCap.Location = new System.Drawing.Point(497, 384);
             this.lbPhuCap.Name = "lbPhuCap";
-            this.lbPhuCap.Size = new System.Drawing.Size(113, 29);
+            this.lbPhuCap.Size = new System.Drawing.Size(94, 25);
             this.lbPhuCap.TabIndex = 18;
             this.lbPhuCap.Text = "Unknown";
             // 
             // lbLuong
             // 
             this.lbLuong.AutoSize = true;
-            this.lbLuong.Location = new System.Drawing.Point(540, 408);
+            this.lbLuong.Location = new System.Drawing.Point(480, 326);
             this.lbLuong.Name = "lbLuong";
-            this.lbLuong.Size = new System.Drawing.Size(113, 29);
+            this.lbLuong.Size = new System.Drawing.Size(94, 25);
             this.lbLuong.TabIndex = 17;
             this.lbLuong.Text = "Unknown";
             // 
             // lbMaNQL
             // 
             this.lbMaNQL.AutoSize = true;
-            this.lbMaNQL.Location = new System.Drawing.Point(561, 341);
+            this.lbMaNQL.Location = new System.Drawing.Point(499, 273);
             this.lbMaNQL.Name = "lbMaNQL";
-            this.lbMaNQL.Size = new System.Drawing.Size(113, 29);
+            this.lbMaNQL.Size = new System.Drawing.Size(94, 25);
             this.lbMaNQL.TabIndex = 16;
             this.lbMaNQL.Text = "Unknown";
             // 
             // lbVaiTro
             // 
             this.lbVaiTro.AutoSize = true;
-            this.lbVaiTro.Location = new System.Drawing.Point(541, 271);
+            this.lbVaiTro.Location = new System.Drawing.Point(481, 217);
             this.lbVaiTro.Name = "lbVaiTro";
-            this.lbVaiTro.Size = new System.Drawing.Size(113, 29);
+            this.lbVaiTro.Size = new System.Drawing.Size(94, 25);
             this.lbVaiTro.TabIndex = 15;
             this.lbVaiTro.Text = "Unknown";
             // 
             // lbGioiTinh
             // 
             this.lbGioiTinh.AutoSize = true;
-            this.lbGioiTinh.Location = new System.Drawing.Point(148, 341);
+            this.lbGioiTinh.Location = new System.Drawing.Point(132, 273);
             this.lbGioiTinh.Name = "lbGioiTinh";
-            this.lbGioiTinh.Size = new System.Drawing.Size(113, 29);
+            this.lbGioiTinh.Size = new System.Drawing.Size(94, 25);
             this.lbGioiTinh.TabIndex = 12;
             this.lbGioiTinh.Text = "Unknown";
             // 
             // lbTen
             // 
             this.lbTen.AutoSize = true;
-            this.lbTen.Location = new System.Drawing.Point(109, 271);
+            this.lbTen.Location = new System.Drawing.Point(97, 217);
             this.lbTen.Name = "lbTen";
-            this.lbTen.Size = new System.Drawing.Size(113, 29);
+            this.lbTen.Size = new System.Drawing.Size(94, 25);
             this.lbTen.TabIndex = 11;
             this.lbTen.Text = "Unknown";
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(451, 341);
+            this.label9.Location = new System.Drawing.Point(401, 273);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(108, 29);
+            this.label9.Size = new System.Drawing.Size(92, 25);
             this.label9.TabIndex = 9;
             this.label9.Text = "Mã NQL:";
             // 
             // lbql6
             // 
             this.lbql6.AutoSize = true;
-            this.lbql6.Location = new System.Drawing.Point(451, 271);
+            this.lbql6.Location = new System.Drawing.Point(401, 217);
             this.lbql6.Name = "lbql6";
-            this.lbql6.Size = new System.Drawing.Size(87, 29);
+            this.lbql6.Size = new System.Drawing.Size(74, 25);
             this.lbql6.TabIndex = 8;
             this.lbql6.Text = "Vai trò:";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::PHANQUYENADMIN.Properties.Resources.Multimedia__253_;
-            this.pictureBox1.Location = new System.Drawing.Point(280, 28);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox1.Location = new System.Drawing.Point(249, 22);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(158, 156);
+            this.pictureBox1.Size = new System.Drawing.Size(140, 125);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 7;
             this.pictureBox1.TabStop = false;
@@ -299,100 +291,99 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(451, 408);
+            this.label7.Location = new System.Drawing.Point(401, 326);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(86, 29);
+            this.label7.Size = new System.Drawing.Size(73, 25);
             this.label7.TabIndex = 6;
             this.label7.Text = "Lương:";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(451, 480);
+            this.label6.Location = new System.Drawing.Point(401, 384);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(106, 29);
+            this.label6.Size = new System.Drawing.Size(90, 25);
             this.label6.TabIndex = 5;
             this.label6.Text = "Phụ cấp:";
             // 
             // lbql5
             // 
             this.lbql5.AutoSize = true;
-            this.lbql5.Location = new System.Drawing.Point(43, 554);
+            this.lbql5.Location = new System.Drawing.Point(38, 443);
             this.lbql5.Name = "lbql5";
-            this.lbql5.Size = new System.Drawing.Size(92, 29);
+            this.lbql5.Size = new System.Drawing.Size(77, 25);
             this.lbql5.TabIndex = 4;
             this.lbql5.Text = "Địa chỉ:";
             // 
             // lbql3
             // 
             this.lbql3.AutoSize = true;
-            this.lbql3.Location = new System.Drawing.Point(43, 408);
+            this.lbql3.Location = new System.Drawing.Point(38, 326);
             this.lbql3.Name = "lbql3";
-            this.lbql3.Size = new System.Drawing.Size(125, 29);
+            this.lbql3.Size = new System.Drawing.Size(105, 25);
             this.lbql3.TabIndex = 3;
             this.lbql3.Text = "Ngày sinh:";
             // 
             // lbql2
             // 
             this.lbql2.AutoSize = true;
-            this.lbql2.Location = new System.Drawing.Point(43, 341);
+            this.lbql2.Location = new System.Drawing.Point(38, 273);
             this.lbql2.Name = "lbql2";
-            this.lbql2.Size = new System.Drawing.Size(107, 29);
+            this.lbql2.Size = new System.Drawing.Size(88, 25);
             this.lbql2.TabIndex = 2;
             this.lbql2.Text = "Giới tính:";
             // 
             // lbql1
             // 
             this.lbql1.AutoSize = true;
-            this.lbql1.Location = new System.Drawing.Point(43, 271);
+            this.lbql1.Location = new System.Drawing.Point(38, 217);
             this.lbql1.Name = "lbql1";
-            this.lbql1.Size = new System.Drawing.Size(62, 29);
+            this.lbql1.Size = new System.Drawing.Size(53, 25);
             this.lbql1.TabIndex = 1;
             this.lbql1.Text = "Tên:";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(444, 152);
+            this.label1.Location = new System.Drawing.Point(395, 122);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(58, 29);
+            this.label1.Size = new System.Drawing.Size(51, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "Mã :";
             // 
             // lbSDT
             // 
             this.lbSDT.AutoSize = true;
-            this.lbSDT.Location = new System.Drawing.Point(110, 480);
+            this.lbSDT.Location = new System.Drawing.Point(98, 384);
             this.lbSDT.Name = "lbSDT";
-            this.lbSDT.Size = new System.Drawing.Size(113, 29);
+            this.lbSDT.Size = new System.Drawing.Size(94, 25);
             this.lbSDT.TabIndex = 20;
             this.lbSDT.Text = "Unknown";
             // 
             // lbNgaySinh
             // 
             this.lbNgaySinh.AutoSize = true;
-            this.lbNgaySinh.Location = new System.Drawing.Point(168, 408);
+            this.lbNgaySinh.Location = new System.Drawing.Point(149, 326);
             this.lbNgaySinh.Name = "lbNgaySinh";
-            this.lbNgaySinh.Size = new System.Drawing.Size(113, 29);
+            this.lbNgaySinh.Size = new System.Drawing.Size(94, 25);
             this.lbNgaySinh.TabIndex = 13;
             this.lbNgaySinh.Text = "Unknown";
             // 
             // lbDiaChi
             // 
             this.lbDiaChi.AutoSize = true;
-            this.lbDiaChi.Location = new System.Drawing.Point(136, 554);
+            this.lbDiaChi.Location = new System.Drawing.Point(121, 443);
             this.lbDiaChi.Name = "lbDiaChi";
-            this.lbDiaChi.Size = new System.Drawing.Size(113, 29);
+            this.lbDiaChi.Size = new System.Drawing.Size(94, 25);
             this.lbDiaChi.TabIndex = 14;
             this.lbDiaChi.Text = "Unknown";
             // 
             // fThongTinNhanVien_QL
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1154, 834);
+            this.ClientSize = new System.Drawing.Size(1026, 667);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "fThongTinNhanVien_QL";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Thông tin";
